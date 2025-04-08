f5f9d8727c0e81456f498baddaa9ac65acd7f7c2
pandausa1453 <kole.hren101@gmail.com>
2025-04-08 14:04:22 -0400

    Delete uv.config.js

--- uv.config.js
+++
@@ -1,9 +0,0 @@
-self.__uv$config = {
-  prefix: '/test/service/',
-  bare: 'https://uv.bare.workers.dev/',
-  codec: new Codec(),
-  tab: {
-    title: 'Google Docs',
-    icon: 'https://ssl.gstatic.com/docs/doclist/images/mediatype/icon_1_document_x16.png',
-  },
-};
