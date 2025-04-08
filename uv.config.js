1e71b81ae2bde8fd1ea94ce52b70d1d93be63038
pandausa1453 <kole.hren101@gmail.com>
2025-04-08 10:59:33 -0700

    Add files via upload

--- uv.config.js
+++ uv.config.js
@@ -1,20 +1,10 @@
 self.__uv$config = {
-  prefix: '/service/',
-  bare: 'https://uv.bare.workers.dev/',
-  encodeUrl: (url) => __uv$config.prefix + __uv$config.codec.encode(url),
-  decodeUrl: (url) => __uv$config.codec.decode(url),
-  codec: new Codec(),
-  tab: {
-    title: "Google Docs",
-    icon: "https://ssl.gstatic.com/docs/doclist/images/mediatype/icon_1_document_x16.png"
-  }
+    prefix: '/service/',
+    bare: 'https://incog.dev/bare/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: '/uv.handler.js',
+    bundle: '/uv.bundle.js',
+    config: '/uv.config.js',
+    sw: '/uv.sw.js',
 };
-
-class Codec {
-  encode(input) {
-    return btoa(input);
-  }
-  decode(input) {
-    return atob(input.replace(__uv$config.prefix, ''));
-  }
-}
